7a545e3e77b8b5df95f1a14a61b1c20512c5d83e
Hwan <idifms@gmail.com>
2021-09-09 00:54:47 +0900

    BOJ SW Test

---
+++ 21609.py
@@ -0,0 +1,155 @@
+# 꼭 문제를 잘 읽자..
+import sys
+sys.stdin = open('input.txt', 'r')
+from collections import deque
+
+N, M = map(int, input().split())
+arr_map = [list(map(int, input().split())) for _ in range(N)]
+
+# print(arr_map)
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+
+def find_block(arr_block):
+    is_visited = [[False]*N for _ in range(N)]
+    color_rep = [-1,-1,0,0]      # i, j, color_cnt, rainbow_cnt
+    list_not_color = [-2, -1, 0]
+
+    for i in range(N):
+        for j in range(N):
+            if arr_block[i][j] in list_not_color:
+                continue
+            if is_visited[i][j]:
+                continue
+
+            is_visited_rain = [[False]*N for _ in range(N)]
+            color = arr_block[i][j]
+            color_cnt = 1
+            rainbow_cnt = 0
+
+            Q = deque()
+            Q.append([i, j])
+            is_visited[i][j] = True
+
+            while Q:
+                x, y = Q.popleft()
+
+                for f_dir in range(4):
+                    nx, ny = x+dx[f_dir], y+dy[f_dir]
+                    if not (0 <= nx < N and 0 <= ny < N):
+                        continue
+
+                    if arr_block[nx][ny] == color and not is_visited[nx][ny]:
+                        Q.append([nx, ny])
+                        color_cnt += 1
+                        is_visited[nx][ny] = True
+                    elif arr_block[nx][ny] == 0 and not is_visited_rain[nx][ny]:
+                        Q.append([nx, ny])
+                        color_cnt += 1
+                        rainbow_cnt += 1
+                        is_visited_rain[nx][ny] = True
+
+            if color_cnt >= 2:
+                if color_rep[2] < color_cnt:
+                    color_rep = [i, j, color_cnt, rainbow_cnt]
+                elif color_rep[2] == color_cnt:
+                    if color_rep[3] < rainbow_cnt:
+                        color_rep = [i, j, color_cnt, rainbow_cnt]
+                    elif color_rep[3] == rainbow_cnt:
+                        if color_rep[0] < i:
+                            color_rep = [i, j, color_cnt, rainbow_cnt]
+                        elif color_rep[0] == i:
+                            if color_rep[1] < j:
+                                color_rep = [i, j, color_cnt, rainbow_cnt]
+
+    return color_rep
+
+
+def delete_block(s_x,s_y,_1,_2):      # -2 는 deleted_value
+    # print(x,y,num)
+    Q = deque()
+    Q.append([s_x,s_y])
+    color = arr_map[s_x][s_y]
+    arr_map[s_x][s_y] = -2
+
+    while Q:
+        x, y = Q.popleft()
+        for f_dir in range(4):
+            nx, ny = x + dx[f_dir], y + dy[f_dir]
+            if not (0 <= nx < N and 0 <= ny < N):
+                continue
+            if arr_map[nx][ny] in [0, color]:
+                arr_map[nx][ny] = -2
+                Q.append([nx, ny])
+
+    return 0
+
+
+def gravity(arr_map):
+    """"
+    가장 큰 row부터 col은 고정 2pos로 탐색
+    위 값이 -1이면 down값은 pos_up -1의 위치로 옮긴다.
+    위값이 비어있으면 -1반복
+    위값이 있으면, down으로 이동.
+    """
+    for j in range(N):
+        for pos_down in range(N-1, 0, -1):
+            if arr_map[pos_down][j] != -2:
+                continue
+
+            for pos_up in range(pos_down-1, -1, -1):
+                tmp_up = arr_map[pos_up][j]
+                if tmp_up == -1:
+                    break
+                elif tmp_up == -2:
+                    continue
+                elif tmp_up >= 0:
+                    arr_map[pos_down][j] = arr_map[pos_up][j]
+                    arr_map[pos_up][j] = -2
+                    break
+    return 0
+
+
+def rotation(arr_map):
+    """
+    격자 반시계 90도 회전
+
+    :param arr_map:
+    :return:    90도 회전한 새로운 N*N list
+    """
+    new_map = [[0]*N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            new_map[N-j-1][i] = arr_map[i][j]
+    return new_map
+
+
+def print_map(arr):
+    for ls in arr:
+        print(*ls, sep='\t')
+    print()
+    return 0
+
+
+ans = 0
+while True:
+    represent_val = find_block(arr_map)  # 대표값 i,j, 개수
+    if not represent_val[2]:
+        break
+    #print(represent_val)
+
+    ans += represent_val[2]**2
+    delete_block(*represent_val)
+    #print_map(arr_map)
+
+    gravity(arr_map)
+    #print_map(arr_map)
+
+    arr_map = rotation(arr_map)
+    #print_map(arr_map)
+
+    gravity(arr_map)
+    #print_map(arr_map)
+
+print(ans)
---
+++ 21611.py
@@ -0,0 +1,166 @@
+# 1 10 27 52        왼
+#  9  17 25
+# 7+2,14+3,21+4
+
+# 0, 7, 22, 45      위
+#  7, 15,  23
+#  7+0, 14+1, 21+2
+
+# 0, 5, 18, 39      오
+#  5, 13, 21
+#  0+5, 7+6, 14+7,
+
+# 0, 3, 14, 33      아
+#   3, 11, 19
+#  0+3, 7+4, 14+5
+
+import sys
+sys.stdin = open('input.txt', 'r')
+
+# 오른, 아래, 왼, 위
+dx, dy = [0,1,0,-1], [1,0,-1,0]
+
+
+def change_arr_to_list(arr):
+    pos_x, pos_y = 0,0
+    is_visited = [[False]*N for _ in range(N)]
+    f_dir = 0
+    ls_map = []
+    for _ in range(N**2):
+        ls_map.append(arr[pos_x][pos_y])
+        is_visited[pos_x][pos_y] = True
+
+        if f_dir == 0:
+            if pos_y+1 == N or is_visited[pos_x][pos_y+1]:
+                f_dir += 1
+        elif f_dir == 1:
+            if pos_x + 1 == N or is_visited[pos_x+1][pos_y]:
+                f_dir += 1
+        elif f_dir == 2:
+            if pos_y - 1 == -1 or is_visited[pos_x][pos_y-1]:
+                f_dir += 1
+        elif f_dir == 3:
+            if pos_x - 1 == -1 or is_visited[pos_x-1][pos_y]:
+                f_dir = 0
+        pos_x, pos_y = pos_x+dx[f_dir], pos_y+dy[f_dir]
+    return list(reversed(ls_map))
+
+
+def break_ball(ls_map, magic_dir, magic_dis):
+    # print(ls_map, magic_dir, magic_dis)
+    idx = 0
+    cnt = 0     # 몇번째 수인지
+    while idx < N**2 and cnt <= magic_dis:
+        ls_map[idx] = 0
+        if magic_dir == 1:      # 위
+            # 0, 7, 22, 45, 76      위
+            #  7, 15, 23, 31
+            # 7+0,14+1,21+2,28+3
+            idx += (7*(cnt+1)+cnt)
+        elif magic_dir == 2:    # 아래
+            # 0, 3, 14, 33      아
+            #   3, 11, 19
+            #  0+3, 7+4, 14+5
+            idx += (7 * cnt + cnt+3)
+        elif magic_dir == 3:    # 왼
+            # 0 1 10 27 52        왼
+            #  1 9  17 25
+            # 0+1,7+2,14+3,21+4
+            idx += (7 * cnt + cnt+1)
+        elif magic_dir == 4:    # 오
+            # 0, 5, 18, 39, 68      오
+            #  5, 13, 21, 29
+            #  0+5, 7+6, 14+7, 21 + 8
+            idx += (7 * cnt + cnt+5)
+        cnt += 1
+    #print(magic_dis, cnt)
+
+    return ls_map
+
+
+def delete_zero(ls_map):
+    # print(ls_map)
+    new_ls = [0]*N**2
+    idx_non_zero = 1
+    for idx in range(1, N**2):
+        if ls_map[idx] != 0:
+            new_ls[idx_non_zero] = ls_map[idx]
+            idx_non_zero += 1
+
+    return new_ls
+
+
+def delete_continuous_ball(ls_map):
+    #print(ls_map)
+    new_ls = [0]*N**2
+    pos_prev = 1
+    cnt = 1
+
+    ball_cnt = 0
+
+    for idx in range(2, N**2):
+        if ls_map[idx] == ls_map[pos_prev]:
+            cnt += 1
+        else:
+            if cnt >= 4:
+                ball_cnt += ls_map[pos_prev]*cnt
+                cnt = 1
+                pos_prev = idx
+            else:
+                new_ls[pos_prev:pos_prev+cnt] = ls_map[pos_prev:pos_prev+cnt]
+                cnt = 1
+                pos_prev = idx
+
+
+    #print(new_ls)
+    return new_ls, ball_cnt
+
+
+def change_to_group(ls_map):
+    new_ls = [0]
+    pos_prev = 1
+    cnt = 1
+
+    for idx in range(2, N ** 2):
+        if ls_map[idx] == ls_map[pos_prev]:
+            cnt += 1
+        else:
+            new_ls.append(cnt)
+            new_ls.append(ls_map[pos_prev])
+            cnt = 1
+            pos_prev = idx
+        if len(new_ls) > N ** 2:
+            new_ls = new_ls[:N ** 2]
+            break
+    new_ls += [0]*(N**2-len(new_ls))
+    # print(new_ls)
+    return new_ls
+
+N, M = map(int, input().split())
+arr_map = [list(map(int,input().split())) for _ in range(N)]
+# print(*arr_map, sep='\n')
+arr_command = [list(map(int,input().split())) for _ in range(M)]
+# print(*arr_command, sep='\n')
+
+ls_map = change_arr_to_list(arr_map)
+# print(ls_map)
+ans = 0
+#print(arr_command)
+
+is_test = False
+
+for i in range(M):
+    ls_map = break_ball(ls_map, *arr_command[i])
+    ls_map = delete_zero(ls_map)
+
+    # ans_tmp = 0
+    while True:
+        ls_map, ans_tmp = delete_continuous_ball(ls_map)
+        if not ans_tmp:
+            break
+        ans += ans_tmp
+        ls_map = delete_zero(ls_map)
+
+    ls_map = change_to_group(ls_map)
+
+print(ans)
